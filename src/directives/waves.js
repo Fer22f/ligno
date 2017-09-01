15df9eba01f838b9a7f7976e1cd33201b3d4cfb5
fer22f <fer22f@gmail.com>
2017-09-01 11:06:01 -0300

    Add wave to ligno

--- src/directives/waves.js
+++ src/directives/waves.js
@@ -1,13 +1,18 @@
 /* @flow */
-import Vue from 'vue'
+import type Vue from 'vue'
+
 import Waves from 'node-waves'
 import 'node-waves/dist/waves.css'
 
-Vue.directive('waves', {
-  inserted (el, bind) {
-    let classes = Object.keys(bind.modifiers).map(val => 'waves-' + val)
-    Waves.attach(el, classes)
-  }
-})
+export default {
+  install (Vue: Vue) {
+    Vue.directive('waves', {
+      inserted (el, bind) {
+        let classes = Object.keys(bind.modifiers).map(val => 'waves-' + val)
+        Waves.attach(el, classes)
+      }
+    })
 
-Waves.init()
+    Waves.init()
+  }
+}
