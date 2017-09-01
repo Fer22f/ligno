5036f657426ed956e623e31c25dca029a8de0f80
fer22f <fer22f@gmail.com>
2017-09-01 11:12:44 -0300

    Add test data to test tables in App

--- docs-src/App.vue
+++ docs-src/App.vue
@@ -1,19 +1,63 @@
 <template lang="pug">
-div
-  | Hello World
-  text-button Testing!
+div#app
+  .top
+    text-box(v-model="value" label="Very nice!")
+  data-table(:columns="columns" :value="data" :selectionMode="false")
+    template(scope="$")
+      td {{$.row.Id}}
+      td {{$.row.Name}}
+      td {{$.row.Surname}}
+      td {{$.row.CreationDate}}
+      td {{$.row.Birthday}}
+      td {{$.row.LuckyNumber}}
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      columns: [
+        { name: 'Id', type: 'num' },
+        { name: 'Name' },
+        { name: 'Surname' },
+        { name: 'Creation Date' },
+        { name: 'Birthday' },
+        { name: 'Lucky Number' }
+      ],
+      data: [...Array(930).keys()].map(i => ({
+        Id: i,
+        Name: 'Facetious',
+        Surname: 'Magnifecent',
+        CreationDate: new Date(Math.random() * 1000000000000),
+        Birthday: new Date(Math.random() * 1000000000000),
+        LuckyNumber: Math.random() * 1000
+      })),
+      value: ''
+    }
+  }
+}
 </script>
 
-<style>
-#app {
-  font-family: 'Verdana', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import '../src/UI.sass';
+
+#app
+  font-family: 'Roboto'
+  display: flex
+  flex: 1
+  flex-flow: column
+
+#app
+  > .datatable
+    flex: 1
+  > .top
+    :padding 8px
+    > *
+    :padding 8px
+
+html, body
+  :width 100%
+  :height 100%
+  :display flex
+  :margin 0
 </style>
